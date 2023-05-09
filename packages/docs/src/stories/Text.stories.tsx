972cbfeef5ff3d05571c3c4e6e664a6bad8450df
Bruno Simplicio <bruno@notabene.id>
2023-05-08 22:33:04 -0400

    using storybook

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque id nemo quidem iste in a laboriosam minima harum similique minus ab, quas, iure et nobis ullam optio. Quod, maxime tenetur?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
